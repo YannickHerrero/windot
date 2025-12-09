9f0e8c2e86a6a48b1b30291e977df0800f469fba
Yannick <yannick.herrero@acelys.fr>
2025-12-09 14:12:03 +0100

    Increase WezTerm font size to 14

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -15,6 +15,7 @@ end)
 return {
   front_end = "OpenGL",
   font = wezterm.font("JetBrainsMono Nerd Font Mono"),
+  font_size = 14,
   color_scheme = "Catppuccin Mocha",
   window_decorations = "RESIZE",
   hide_tab_bar_if_only_one_tab = true,
